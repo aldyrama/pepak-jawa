c38996267b867b2166f0c102c0d4983ae78773b9
Aldy Ramadhan <aldy.ramadhan012@gmail.com>
2018-09-23 07:08:31 +0700

    commit revision

---
+++ app/src/main/java/org/d3ifcool/pepakjawa/RupaKawruhActivity.java
@@ -0,0 +1,29 @@
+package org.d3ifcool.pepakjawa;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RupaKawruhActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rupa_kawruh);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/org/d3ifcool/pepakjawa/WayangActivity.java
@@ -0,0 +1,19 @@
+package org.d3ifcool.pepakjawa;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class WayangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wayang);
+
+    }
+
+}
