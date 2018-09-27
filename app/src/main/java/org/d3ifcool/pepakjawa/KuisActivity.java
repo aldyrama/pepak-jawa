4d58df788840318d07d175098fabb8a77135bdf0
Sri Ayu Miati <ayumisetyo@gmail.com>
2018-09-27 22:46:03 +0700

    Edit Layout Kuis and Kuis Activity

--- app/src/main/java/org/d3ifcool/pepakjawa/KuisActivity.java
+++ app/src/main/java/org/d3ifcool/pepakjawa/KuisActivity.java
@@ -19,9 +19,17 @@ import com.google.android.gms.common.internal.Objects;
 
 public class KuisActivity extends AppCompatActivity {
 
-    AlertDialog.Builder builder;
-    RadioGroup radiogroup;
+    private KuisBank mQuestionLibrary = new KuisBank();
+    private TextView mScoreView;
+    private TextView mQuestionView;
+    private Button mButtonChoiche1;
+    private Button mButtonChoiche2;
+    private Button mButtonChoiche3;
+    private Button mButtonChoiche4;
 
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,42 +39,47 @@ public class KuisActivity extends AppCompatActivity {
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
-        radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
-
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestionView = (TextView)findViewById(R.id.questions);
+        mButtonChoiche1 = (Button)findViewById(R.id.choice1);
+        mButtonChoiche2 = (Button)findViewById(R.id.choice2);
+        mButtonChoiche3 = (Button)findViewById(R.id.choice3);
+        mButtonChoiche4 = (Button)findViewById(R.id.choice4);
+        updateQuestion();
+        updateScore(mScore);
     }
 
-    public void onRadioButton (View view) {
-        Boolean checked = ((RadioButton) view).isChecked();
-
-        switch (view.getId()) {
-            case R.id.radioButton:
-                if (checked)
-                    jawabanSalah();
-                break;
-            case R.id.radioButton2:
-                if (checked)
-                    jawabanSalah();
-                break;
-            case R.id.radioButton3:
-                if (checked)
-                    tampilDialog();
-                break;
-            case R.id.radioButton4:
-                if (checked)
-                    jawabanSalah();
-                break;
+    private void updateQuestion() {
+        if (mQuestionNumber < mQuestionLibrary.getLength()) {
+            mQuestionView.setText(mQuestionLibrary.getQuestion(mQuestionNumber));
+            mButtonChoiche1.setText(mQuestionLibrary.getChoice(mQuestionNumber, 1));
+            mButtonChoiche2.setText(mQuestionLibrary.getChoice(mQuestionNumber, 2));
+            mButtonChoiche3.setText(mQuestionLibrary.getChoice(mQuestionNumber, 3));
+            mButtonChoiche4.setText(mQuestionLibrary.getChoice(mQuestionNumber, 4));
+            mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
+            mQuestionNumber++;
+        } else {
+            Toast.makeText(KuisActivity.this, "It was the last question!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(KuisActivity.this, ScoreActivity.class);
+            intent.putExtra("score", mScore); // pass the current score to the second screen
+            startActivity(intent);
         }
     }
-
-    //menampilkan toast salah
-    private void jawabanSalah() {
-        Toast.makeText(this, "Jawaban anda Salah", Toast.LENGTH_SHORT).show();
+    private void updateScore(int point) {
+        mScoreView.setText("" + mScore + "/" + mQuestionLibrary.getLength());
     }
 
-    private void tampilDialog() {
-        builder = new AlertDialog.Builder(this);
-        builder.setCancelable(false);
-        builder.setTitle("Selamat!");
-        builder.setMessage("Jawaban anda benar");
+    public void onClick(View view) {
+        //all logic for all answers buttons in one method
+        Button answer = (Button) view;
+        // if the answer is correct, increase the score
+        if (answer.getText() == mAnswer){
+            mScore = mScore + 1;
+            Toast.makeText(KuisActivity.this, "Benar!", Toast.LENGTH_SHORT).show();
+        }else
+            Toast.makeText(KuisActivity.this, "Salah!", Toast.LENGTH_SHORT).show();
+        updateScore(mScore);
+        updateQuestion();
+
     }
 }
